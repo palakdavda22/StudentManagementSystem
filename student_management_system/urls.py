1b26f763e5bcc6b1116abe025e169d0aa6bbadf6
palak davda <palakdavda2202@gmail.com>
2021-05-13 19:17:14 +0530

    First Commit

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -51,5 +51,7 @@ urlpatterns = [
 # #     Staff URL Path
     path('staff_home', StaffViews.staff_home, name="staff_home"),
     path('student_home', StudentViews.student_home, name="student_home"),
+    path('student_view_attendance', StudentViews.student_view_attendance, name="student_view_attendance"),
+
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
